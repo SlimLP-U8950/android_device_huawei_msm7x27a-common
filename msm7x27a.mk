96286e709a9d28a440e851a2ec3f7f84fb71032a
alexey podvoyskiy <fell978@yandex.ru>
2016-01-26 20:09:57 +0300

    dalvik update for 768mb ram

--- msm7x27a.mk
+++ msm7x27a.mk
@@ -146,8 +146,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	pm.sleep_mode=4
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	dalvik.vm.heapgrowthlimit=52m \
-	dalvik.vm.heaptargetutilization=0.25
+    dalvik.vm.heapstartsize=5m \
+    dalvik.vm.heapgrowthlimit=64m \
+    dalvik.vm.heapsize=128m \
+    dalvik.vm.heaptargetutilization=0.25 \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapmaxfree=2m
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	com.qc.hardware=true \

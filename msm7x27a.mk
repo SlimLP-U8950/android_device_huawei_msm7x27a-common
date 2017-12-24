85abc49dda642e81902507567ce5e5f0d6116cc8
alexey podvoyskiy <fell978@yandex.ru>
2017-12-24 17:05:11 +0300

    Update msm7x27a.mk

--- msm7x27a.mk
+++ msm7x27a.mk
@@ -41,7 +41,8 @@ PRODUCT_PACKAGES += \
     gralloc.msm7x27a \
     copybit.msm7x27a \
     libqdMetaData \
-    memtrack.msm7x27a
+    memtrack.msm7x27a \
+    hwcomposer.msm7x27a
 
 PRODUCT_PACKAGES += \
     libmm-omxcore \
